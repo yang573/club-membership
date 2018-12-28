91df9ced24a7d905485e68be42a5c1cf9047c380
Yang Yang <yangyang1998@gmail.com>
2018-12-28 21:50:08 +0100

    Updated mock data sql script

--- server/mock_data.sql
+++ server/mock_data.sql
@@ -1,6 +1,11 @@
 Use dev_sbcs;
 
-Insert into Members (FirstName, LastName, YearID, Email, Newsletter)
-Values
-	('Yang', 'Yang', 1, 'yang.yang.7@stonybrook.edu', true),
-	('Joe', 'Wilson', 1, 'joseph.wilson@stonybrook.edu', true);
+INSERT INTO Members (FirstName, LastName, YearID, Email, Newsletter)
+VALUES
+	('Brianna', 'Florio', 2, 'brianna.florio@stonybrook.edu', true),
+	('Joe', 'Wilson', 2, 'joseph.wilson@stonybrook.edu', true),
+	('Yang', 'Yang', 2, 'yang.yang.7@stonybrook.edu', true),
+    ('John', 'Doe', 4, 'john.doe@notreal.edu', false);
+
+INSERT INTO Members (FirstName, LastName, YearID, Email, Newsletter)
+VALUES ('John', 'Moe', 3, 'john.moe@notreal.edu', false);
