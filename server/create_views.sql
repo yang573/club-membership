196092e7acb586398a8773908e7ede8d66955a7e
Yang Yang <yangyang1998@gmail.com>
2018-12-30 18:25:07 +0100

    Added sql scripts for creating views

---
+++ server/create_views.sql
@@ -0,0 +1,40 @@
+USE dev_sbcs;
+
+CREATE VIEW Membership AS SELECT
+	base.*,
+    CASE WHEN Semester_Attendance > 2
+		THEN true
+        ELSE false
+		END AS Active
+FROM(
+SELECT
+	Members.FirstName,
+	Members.LastName,
+    (SELECT COUNT(*) FROM Events WHERE
+		SemesterID = (SELECT SemesterID FROM Semester ORDER BY SemesterID DESC LIMIT 1) AND
+		EventID IN (SELECT EventID FROM Member_Event WHERE Member_Event.MemberID = Members.MemberID))
+		AS Semester_Attendance,
+	(SELECT COUNT(*) FROM Member_Event WHERE Member_Event.MemberID = Members.MemberID)
+		AS Total_Attendance
+FROM Members
+) AS base;
+
+
+CREATE VIEW Recent_Events AS SELECT
+	EventID, Name, Date, Attendance
+FROM Events
+WHERE SemesterID = (SELECT SemesterID FROM Semester ORDER BY SemesterID DESC LIMIT 1);
+
+
+CREATE VIEW Semester_Overview AS SELECT
+	base.*,
+    (SELECT COUNT(*) FROM Events WHERE Events.SemesterID = ID)
+		AS Number_Of_Events,
+	(SELECT ROUND(AVG(Attendance), 2) FROM Events WHERE Events.SemesterID = ID)
+		AS Average_Attendance
+FROM (
+SELECT
+	Semester.Value AS Name,
+    Semester.SemesterID AS ID
+FROM Semester
+) AS base;
