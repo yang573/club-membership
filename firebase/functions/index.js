c403042e6218e12d7dd09947bd728e252d425b33
Jaw0608 <joseph.wilson@stonybrook.edu>
2019-01-16 11:08:00 -0500

    added firebase setup

---
+++ firebase/functions/index.js
@@ -0,0 +1,18 @@
+const functions = require('firebase-functions');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+//email should be identifier.
+exports.pullMembers = functions.https.onCall((data, context)->{
+
+)};
+
+//List of needed Functions for stats
+// 1) Pull members/event details from spreadsheets
+// 2) Calculate number of active members
+// 3) Get statistics data on any person (list of events attended, active status)
