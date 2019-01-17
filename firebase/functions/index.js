da3ec761fd1cd526649992cc08e15e194855bd4c
Jaw0608 <joseph.wilson@stonybrook.edu>
2019-01-17 15:40:06 -0500

    Index.js now holds our function to pull all spreadsheets and get their contents

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,4 +1,16 @@
 const functions = require('firebase-functions');
+var {
+  google
+} = require('googleapis');
+let privatekey = require("./privatekey.json");
+let jwtClient = new google.auth.JWT(
+  privatekey.client_email,
+  null,
+  privatekey.private_key,
+  ['https://www.googleapis.com/auth/spreadsheets',
+    'https://www.googleapis.com/auth/drive',
+    'https://www.googleapis.com/auth/calendar'
+  ]);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -8,11 +20,63 @@ const functions = require('firebase-functions');
 // });
 
 //email should be identifier.
-exports.pullMembers = functions.https.onCall((data, context)->{
-
-)};
+exports.pullMemberData = functions.https.onCall((data, context) => {
+    jwtClient.authorize(function(err, tokens) {
+      if (err) {
+        console.log(err);
+        return;
+      } else {
+        console.log("Successfully connected to sheets");
+        var parentID = "1lCoITNE0eyrRNxKtVhpHe7bq-P0FUp0e";
+        loadFiles(jwtClient, parentID);
+      }
+    });
+});
 
 //List of needed Functions for stats
 // 1) Pull members/event details from spreadsheets
 // 2) Calculate number of active members
 // 3) Get statistics data on any person (list of events attended, active status)
+
+//drive api stuff
+function getSheetData(auth,sheetID){
+const sheets = google.sheets({version: 'v4', auth});
+//var spreadsheetId = "1sSDRdukNm1UskZ9ivkuJ2hvsX-HhHLEfxO8rszuoqz8";
+var request = {
+        valueRenderOption: 'FORMATTED_VALUE',
+        spreadsheetId:sheetID,
+        range:"A:E"
+}
+sheets.spreadsheets.values.get(request, function(err,response){
+  if (err) {
+      console.error(err);
+      return;
+    }
+
+    // TODO: Change code below to process the `response` object:
+    console.log(response.data.values);
+  });
+}
+function loadFiles(auth,parent) {
+  const drive = google.drive({version: 'v3', auth});
+  drive.files.list({
+    fields: 'nextPageToken, files(id, name)',
+                q: "'"+parent+"' in parents",
+  }, (err, res) => {
+  //  if (err) console.log('The API returned an error when checking file: ' + err);
+  if(err) {
+    console.log(err.errors);
+    return;
+  }
+                if (res.data.files===null) {
+                        console.log("No files found");
+                        return;
+                }
+    const files = res.data.files;
+    if (files.length) {
+        files.forEach(function(file){
+          getSheetData(auth,file.id);
+        });
+    }
+  });
+}
---
+++ firebase/functions/savetosheet.js
@@ -0,0 +1,64 @@
+// //#!/usr/local/bin/node
+// var {google} = require('googleapis');
+// let privatekey = require("./privatekey.json");
+// let jwtClient = new google.auth.JWT(
+//                        privatekey.client_email,
+//                        null,
+//                        privatekey.private_key,
+//                        ['https://www.googleapis.com/auth/spreadsheets',
+//                                                        'https://www.googleapis.com/auth/drive',
+//                                                        'https://www.googleapis.com/auth/calendar']);
+// //authenticate request
+// jwtClient.authorize(function (err, tokens) {
+//  if (err) {
+//    console.log(err);
+//          return;
+//  } else {
+//                           console.log("Successfully connected!");
+//                         var parentID = "1lCoITNE0eyrRNxKtVhpHe7bq-P0FUp0e";
+//                         loadFiles(jwtClient, parentID);
+//                                        }
+//                                                   });
+//
+//
+// function getSheetData(auth,sheetID){
+// const sheets = google.sheets({version: 'v4', auth});
+// //var spreadsheetId = "1sSDRdukNm1UskZ9ivkuJ2hvsX-HhHLEfxO8rszuoqz8";
+// var request = {
+//         valueRenderOption: 'FORMATTED_VALUE',
+//         spreadsheetId:sheetID,
+//         range:"A:E"
+// }
+// sheets.spreadsheets.values.get(request, function(err,response){
+//   if (err) {
+//       console.error(err);
+//       return;
+//     }
+//
+//     // TODO: Change code below to process the `response` object:
+//     console.log(response.data.values);
+//   });
+// }
+// function loadFiles(auth,parent) {
+//   const drive = google.drive({version: 'v3', auth});
+//   drive.files.list({
+//     fields: 'nextPageToken, files(id, name)',
+//                 q: "'"+parent+"' in parents",
+//   }, (err, res) => {
+//   //  if (err) console.log('The API returned an error when checking file: ' + err);
+//   if(err) {
+//     console.log(err.errors);
+//     return;
+//   }
+//                 if (res.data.files==null) {
+//                         console.log("No files found");
+//                         return;
+//                 }
+//     const files = res.data.files;
+//     if (files.length) {
+//         files.forEach(function(file){
+//           getSheetData(auth,file.id);
+//         });
+//     }
+//   });
+// }
